8ab40b0470df6325dc359d848f0421042a58448f
Yulia <yulia.v@Yulias-MacBook.local>
2021-10-16 01:02:12 +0700

    feat:complete st-queue task

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,20 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
+  constructor() {
+    this.value = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this;
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value, currentThis = this) {
+    console.log(currentThis);
+    if (currentThis.next === undefined) {
+      this.value = value;
+      this.next = null;
+      return;
+    }
+    if (currentThis.next !== null) {
+      this.enqueue(value, currentThis.next);
+    } else if (currentThis.next === null) {
+      currentThis.next = {};
+      currentThis = currentThis.next;
+      currentThis.value = value;
+      currentThis.next = null;
+      return;
+    }
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const valueFirst = this.value;
+    const first = this.next;
+    console.log(valueFirst, first);
+    this.value = first.value;
+    this.next = first.next;
+    return valueFirst;
   }
-
-}
+};
